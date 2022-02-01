c24b79f3ab7cf527aee13c974f2bba111c76ef9c
AnneLaure Le <annelaurele@hotmail.fr>
2022-02-01 21:13:55 +0100

    erreur

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Castle.create(address: 'Toulouse', title: 'Majestueux château aux abords de la ville', price: '550€', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', max_capacity: 10)
