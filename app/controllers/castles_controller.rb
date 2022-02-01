b151fadc493780bf6336175c94425013f015aa7d
Julia Foucher <julia814@hotmail.fr>
2022-02-01 21:00:23 +0100

    route et controller castle

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -1,2 +1,18 @@
 class CastlesController < ApplicationController
+
+  def index
+    @castles = Castle.all
+  end
+
+  def create
+    @castle = Castle.new
+    @castle.save
+
+    redirect_to castle_path(@castle)
+  end
+
+  def new
+    @castle = Castle.new
+  end
+
 end
