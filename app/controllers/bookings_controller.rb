bc7ee5d3a3df726d603c472f8c12cf1085c94c64
Elliot Thyrant <elliot.thyrant2001@gmail.com>
2022-02-05 12:17:29 +0100

    tmp castle_show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,8 @@
 class BookingsController < ApplicationController
+  def new
+    @castle = Castle.find(params[:castle_id])
+    @booking = Booking.new
+    @booking.user = current_user
+    @booking.castle = @castle
+  end
 end
