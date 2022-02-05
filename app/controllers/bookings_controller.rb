475d91dc6306e06ac1b0250a8b353b94fefa424f
Elliot Thyrant <elliot.thyrant2001@gmail.com>
2022-02-05 13:05:39 +0100

    tmp booking_new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,7 +2,23 @@ class BookingsController < ApplicationController
   def new
     @castle = Castle.find(params[:castle_id])
     @booking = Booking.new
-    @booking.user = current_user
+  end
+
+  def create
+    @castle = Castle.find(params[:castle_id])
+    @booking = Booking.new(booking_params)
     @booking.castle = @castle
+    @booking.user = current_user
+    if @booking.save
+      redirect_to castle_path(@castle)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:arrival_date, :departure, :number_of_people, :status, :user_id, :castle_id)
   end
 end
