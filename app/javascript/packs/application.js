fc6e2f94cefb3af35d73277ec3fd7eeeaee35bba
Julia Foucher <julia814@hotmail.fr>
2022-02-05 15:43:13 +0100

    map sur index castle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -39,7 +39,8 @@ flatpickr("datepicker", {
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
+import { initMapbox } from '../plugins/init_mapbox';
+
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
+  initMapbox();
+})
